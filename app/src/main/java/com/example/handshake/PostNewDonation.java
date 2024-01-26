1fd66a3a8c5b9718012c3db8c57ac67e22cae321
MayaHayat <maya.hayat@gmail.com>
2024-01-26 23:13:47 +0200

    Donations realtime works

--- app/src/main/java/com/example/handshake/PostNewDonation.java
+++ app/src/main/java/com/example/handshake/PostNewDonation.java
@@ -1,5 +1,6 @@
 package com.example.handshake;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,12 +9,19 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class PostNewDonation extends AppCompatActivity {
 
@@ -24,6 +32,9 @@ public class PostNewDonation extends AppCompatActivity {
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+
+    // Strings to save the info of the donation:
+    String getDonationName, getDonationInfo, getCatagory, getDonationLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +52,21 @@ public class PostNewDonation extends AppCompatActivity {
         databaseReference = firebaseDatabase.getReference();
 
 
+        createDonation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDonationName = nameDonation.getText().toString();
+                getDonationInfo = infoDonation.getText().toString();
+                getDonationLocation = chooseLocation.getSelectedItem().toString();
+                int selectedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+                RadioButton selectedRadioButton = findViewById(selectedRadioButtonId);
+                getCatagory = selectedRadioButton.getText().toString();
+                saveUserDataToDatabase();
 
+            }
+        });
+
+            // DROP DOWN MENU FOR LOCATION
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this,
                 R.array.regions,
@@ -63,5 +88,32 @@ public class PostNewDonation extends AppCompatActivity {
                 // Do nothing here
             }
         });
+    }
+
+    private void saveUserDataToDatabase() {
+        // Save information to Firebase Database
+        HashMap<String, Object> donationMap = new HashMap<>();
+        donationMap.put("Name", getDonationName);
+        donationMap.put("Info", getDonationInfo);
+        donationMap.put("Catagory", getCatagory);
+        donationMap.put("Location", getDonationLocation);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("User");
+        String userID = user.getUid();
+
+        databaseReference.child("Donations").child(userID).setValue(donationMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(PostNewDonation.this, "Data added", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(PostNewDonation.this, "Couldn't add data", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
